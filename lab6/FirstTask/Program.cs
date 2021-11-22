eca82baad3f41d4a72012380e43d8ebc63b88186
Valeriia Spasibukhova <megalera1913@gmail.com>
2021-11-22 23:03:26 +0200

    Добавьте файлы проекта.

---
+++ lab6/FirstTask/Program.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GenMeth();
+            Console.ReadKey();
+        }
+        public static void GenMeth()
+        {
+
+            AbstractHandler[] handlers = new AbstractHandler[3];
+            Console.WriteLine($"Number of formats allowed = {handlers.Length}");
+
+            for (int i = 0; i < handlers.Length; i++)
+            {
+                Console.WriteLine("Choose the document format: (XML, TXT, DOC)");
+                string choise = Console.ReadLine();
+                Console.Write("\n");
+                if (choise == "XML")
+                {
+                    new XMLHandler(choise).AllMethods();
+                }
+                else if (choise == "TXT")
+                {
+                    new TXTHandler(choise).AllMethods();
+                }
+                else if (choise == "DOC")
+                {
+                    new DOCHandler(choise).AllMethods();
+                }
+                else
+                {
+                    Console.WriteLine("Somethimg wemt wrong. Choose another format..");
+                    break;
+                }
+            }
+
+            Console.WriteLine("Closing....");
+
+        }
+    }
+    abstract class AbstractHandler
+    {
+        public string FormatName { get; set; }
+        public AbstractHandler(string name)
+        {
+            this.FormatName = name;
+        }
+        public abstract void Open();
+        public abstract void Create();
+        public abstract void Change();
+        public abstract void Save();
+    }
+    class Realization : AbstractHandler
+    {
+
+        public Realization(string name)
+            : base(name)
+        { }
+
+        public override void Create()
+        {
+            Console.WriteLine($"Creating {FormatName} document");
+        }
+
+        public override void Open()
+        {
+            Console.WriteLine($"Opening {FormatName} document");
+        }
+        public override void Change()
+        {
+            Console.WriteLine($"Opening {FormatName} document");
+        }
+        public override void Save()
+        {
+            Console.WriteLine($"Saving {FormatName} document");
+        }
+
+        public virtual void AllMethods()
+        {
+            Create();
+            Open();
+            Change();
+            Save();
+        }
+    }
+    class XMLHandler : Realization
+    {
+
+        public XMLHandler(string name)
+            : base(name)
+        { }
+
+        public override void AllMethods()
+        {
+            base.AllMethods();
+        }
+    }
+    class TXTHandler : Realization
+    {
+        public TXTHandler(string name)
+            : base(name)
+        { }
+        public override void AllMethods()
+        {
+            base.AllMethods();
+        }
+
+    }
+    class DOCHandler : Realization
+    {
+        public DOCHandler(string name)
+            : base(name)
+        { }
+        public override void AllMethods()
+        {
+            base.AllMethods();
+        }
+    }
+}
---
+++ lab6/ThirdTask/Program.cs
@@ -0,0 +1,120 @@
+﻿using System;
+
+
+namespace ThirdTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GenMeth();
+            Console.ReadKey();
+
+        }
+        public static void GenMeth()
+        {
+            string [] firstArr = new string[2] { "Book1", "Book2" };
+            string [] secondArr = new string[2] { "Magazine1", "Magazine2" };
+           
+            AllJournals allJournals = new AllJournals(firstArr,secondArr);
+            allJournals.AllJ();
+            allJournals.Info();
+        }
+    }
+    interface Printable
+    {
+        void Print();
+    }
+   
+   
+    class Book:Printable
+    {
+        string [] BooksName { get; set; }
+        public Book(string [] allBooks)
+        {
+            this.BooksName = allBooks;
+        }
+        public void Print()
+        {
+            Console.WriteLine($"First Book - {BooksName[0]}");
+            Console.WriteLine($"First Book - {BooksName[1]}");
+        }
+
+        public void PrintBooks(Printable[] printable)
+        {
+            printable = new Printable[1];
+            printable[0] = new Book(BooksName);
+            foreach (Printable allBooks in printable)
+            {
+                allBooks.Print();
+            }
+        }
+    }
+    class Magazine:Printable
+    {
+        string[] MagazinesName { get; set; }
+        public Magazine(string[] allMagazines)
+        {
+            this.MagazinesName = allMagazines;
+        }
+        public void Print()
+        {
+            Console.WriteLine($"First Magazine - {MagazinesName[0]}");
+            Console.WriteLine($"First Magazine - {MagazinesName[1]}");
+        }
+
+        public void PrintMagazines(Printable[] printable)
+        {
+            printable = new Printable[1];
+            printable[0] = new Magazine(MagazinesName);
+            foreach (Printable allMagazines in printable)
+            {
+                allMagazines.Print();
+            }
+        }
+    }
+    class AllJournals
+    {
+        Printable[] printables = new Printable[2];
+        public string [] jBooks { get; set; }
+        public string[] jMagazines { get; set; }
+
+        public AllJournals(string[] jBooks, string[] jMagazines)
+        {
+            this.jBooks = jBooks;
+            this.jMagazines = jMagazines;
+        }
+      
+        public void AllJ()
+        {
+            Console.WriteLine("ALL BOOKS AND MAGAZINES:");
+            printables[0] = new Book(jBooks);
+            printables[1] = new Magazine(jMagazines);
+
+            foreach (Printable allgetprint in printables)
+            {
+                allgetprint.Print();
+            }
+        }
+        public void Info()
+        {
+            Console.Write("\n");
+            Console.WriteLine("ONLY BOOKS:");
+            Book books = new Book(jBooks);
+            Printable[] printables1 = { books };
+            foreach (Book toprint in printables1)
+            {
+                toprint.PrintBooks(printables1);
+            }
+            Console.Write("\n");
+            Console.WriteLine("ONLY MAGAZINES:");
+            Magazine magazines = new Magazine(jMagazines);
+            Printable[] printables2 = {magazines};
+            foreach (Magazine toprint in printables2)
+            {
+                toprint.PrintMagazines(printables2);
+            }
+        }
+       
+    }
+}
